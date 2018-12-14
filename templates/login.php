6a87bfd5250033269409f5d666927586348be4ca
Jose Lopez <jflo1937@gmail.com>
2018-12-14 13:13:21 -0400

    vistas acomodadas

--- templates/login.php
+++ templates/login.php
@@ -20,22 +20,22 @@
       
             <div style="padding-top:30px" class="panel-body" >
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                    <form  action="../logica/control/IngresoControl.php" method="POST" class="form-horizontal" role="form">
-                        <div style="margin-bottom: 25px" class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username">
+                <form  action="../logica/control/IngresoControl.php" method="POST" class="form-horizontal" role="form">
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username">
+                    </div>
+                    <div style="margin-bottom: 25px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                    </div>
+                    <div style="margin-top:10px" class="form-group">
+                        <div class="col-sm-12 controls">
+                            <input type="submit" class="btn btn-primary" value="Ingresar">
+                            <input type="reset" class="btn btn-success">
                         </div>
-                        <div style="margin-bottom: 25px" class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-                        </div>
-                        <div style="margin-top:10px" class="form-group">
-                            <div class="col-sm-12 controls">
-                                <input type="submit" class="btn btn-primary" value="Ingresar">
-                                <input type="reset" class="btn btn-success">
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                </form>
             </div>                     
         </div>
     </div>
